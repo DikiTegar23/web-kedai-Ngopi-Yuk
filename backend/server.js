fd605d1148c023f41d0c9ceac4a1d41cf2a3b03f
DikiTegar23 <dikitegarr62@gmail.com>
2026-07-12 00:43:09 +0700

    Use environment variable for server port

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import orderRouter from "./routes/orderRoute.js";
 
 // app config
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 // middleware
 app.use(express.json());
@@ -32,8 +32,9 @@ app.get("/",(req,res)=>{
     res.send("API Working");
 })
 
+
 app.listen(port,()=>{
-    console.log(`Server Started on http://localhost:${port} `);
+    console.log(`Server Started on ${port} `);
     
 })
 
